c5628a48c0a078a4e842b41af6529d6008606e64
marcandy <marcandy.nj@gmail.com>
2016-09-21 14:53:21 -0500

    'building chatty app with node and express'

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+/*jshint esversion: 6 */
 
+const app = express();
+const port = 8000;
+
+const messages = [];
+
+app.get('/', (req, res) => {
+  res.send(JSON.stringify(messages));//more about the send property
+});
+
+
+
+app.listen(port)
